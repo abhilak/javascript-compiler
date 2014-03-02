3b34093f22cc6ebd05743c9442c1b900882024e4
Srijan R Shetty <srijan.shetty@gmail.com>
2014-03-02 10:51:52 +0530

    more tokens

--- README.md
+++ README.md
@@ -8,8 +8,16 @@
 
 - 10th March, the AST should be completed by this time.
 
+# Usage
+
+- run python lexer.py <testFileName>
+
 # Build Instruction
 
 - The builds are only for a Linux compliant machine (preferably Ubunutu)
 - Use the makefile provided (instructions will be added as and when new componenets will be added)
+
+# Tests
+
+- All the test files need to added under the tests folder
 
--- lexer.py
+++ lexer.py
@@ -3,23 +3,49 @@ from sys import argv
 
 filename, input_file = argv 
 
-
 # The different token need to be defined as a tuple
 tokens = (
         "KEYWORD",
         "IDENTIFIER",
-        "WHITESPACE"
+        "WHITESPACE",
+        "NUMBER",
+        "OP_ASSIGNMENT",
+        "SEP_SEMICOLON",
+        "SEP_OPEN_BRACE",
+        "SEP_CLOSE_BRACE"
         )
 
 # RegEx for KEYWORDS
 t_KEYWORD = (
         r"var|"
         r"if|"
-        r"else"
+        r"else|"
+        r"for|"
+        r"while|"
+        r"typeof|"
+        r"function|"
+        r"undefined|"
+        r"infinity|"
+        r"null|"
+        r"Array|"
+        r"Object|"
+        r"Function|"
+        r"NaN"
         )
 
 # RegEx for IDENTIFIERS
 t_IDENTIFIER = r"[a-zA-Z]\w+"
+
+# RegEx for NUMBERS
+t_NUMBER = r"\d+(\.\d+)?"
+
+# RegEx for OPERATORS
+t_OP_ASSIGNMENT = r"="
+
+# RegEx for SEPERATORS
+t_SEP_SEMICOLON = r";"
+t_SEP_OPEN_BRACE = r"{"
+t_SEP_CLOSE_BRACE = r"{"
 
 # RegEx for WHITESPACE
 def t_WHITESPACE(t): 
