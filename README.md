a826ecd019beca5314269a1a1fc78f154be4a661
Srijan R Shetty <srijan.shetty@gmail.com>
2014-03-02 12:09:02 +0530

    A description of what has been implemented and what not

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-# Good parts JavaScript Compiler
-- We'll be working on creating a JavaScript good parts compiler.
+# Practical JavaScript Compiler
+- We'll be working on creating a JavaScript practical compiler.
+    - The version we are targeting is ES5.
+    - Constructs which are bad will be omitted.
+        - with
+    - Lesser used constructs will also be omitted.
+        - eval
+        - '==' and '===' mean the same thing
+        - '!=' and '!==' mean the same thing
+    - There is no forwards compatibility.
+        - class
+        - const
+        - default
+        - extends
+        - field
+        - final
+        - import
+        - package
+        - private
+        - protected
+        - public
+        - super
+    - RegEx are not supported as of yet.
+    - Ternary operator is not supported as well.
 - The target architecture is SPIM.
 - The implementation language is python.
 
@@ -16,8 +38,8 @@
 - [ply](https://github.com/dabeaz/ply)
 
 ## Compiling and Building
-- The builds are only for a Linux compliant machine (preferably Ubunutu)
-- Use the makefile provided (instructions will be added as and when new componenets will be added)
+- The builds are only for a Linux compliant machine (preferably Ubuntu)
+- Use the makefile provided (instructions will be added as and when new components will be added)
 
 ## Tests
 - All the test files need to added under the tests folder
--- lexer.py
+++ lexer.py
@@ -10,26 +10,59 @@ tokens = (
         "WHITESPACE",
         "NUMBER",
         "OP_ASSIGNMENT",
+        "OP_COLON",
+        "OP_EQUALS",
+        "OP_NOT_EQUALS",
+        "OP_ADDITION",
+        "OP_SUBTRACTION",
+        "OP_MULTIPLICATION",
+        "OP_DIVISION",
+        "OP_MODULUS",
         "SEP_SEMICOLON",
         "SEP_OPEN_BRACE",
-        "SEP_CLOSE_BRACE"
+        "SEP_CLOSE_BRACE",
+        "SEP_QUOTES",
+        "SEP_OPEN_BRACKET",
+        "SEP_CLOSE_BRACKET"
         )
 
 # RegEx for KEYWORDS
 t_KEYWORD = (
         r"var|"
+        r"let|"
+        # Programming Constructs
         r"if|"
         r"else|"
         r"for|"
+        r"in|"
+        r"do|"
         r"while|"
-        r"typeof|"
+        r"switch|"
+        r"case|"
+        r"break|"
+        r"continue|"
         r"function|"
-        r"undefined|"
-        r"infinity|"
-        r"null|"
+        r"return|"
+        r"try|"
+        r"throw|"
+        r"catch|"
+        r"finally|"
+        # OOP features
+        r"new|"
+        r"this|"
+        r"delete|"
+        # Other operators
+        r"instanceof|"
+        r"typeof|"
+        # Entities in the language
         r"Array|"
         r"Object|"
         r"Function|"
+        r"true|"
+        r"false|"
+        r"undefined|"
+        r"infinity|"
+        r"null|"
         r"NaN"
         )
 
@@ -37,15 +70,45 @@ t_KEYWORD = (
 t_IDENTIFIER = r"[a-zA-Z]\w+"
 
 # RegEx for NUMBERS
-t_NUMBER = r"\d+(\.\d+)?"
+def t_NUMBER(t):
+    r"\d+(\.\d+)?"
+    t.value = float(t.value)
+    return t
 
 # RegEx for OPERATORS
-t_OP_ASSIGNMENT = r"="
+t_OP_ASSIGNMENT = (
+        r"=|"
+        r"\+=|"
+        r"-=|"
+        r"/=|"
+        r"%=|"
+        r"\*="
+        )
+t_OP_COLON = r":"
+t_OP_ADDITION = r"\+"
+t_OP_SUBTRACTION = r"-"
+t_OP_MULTIPLICATION = r"\*"
+t_OP_DIVISION = r"/"
+t_OP_MODULUS = r"%"
+t_OP_EQUALS = (
+        r"==|"
+        r"==="
+        )
+t_OP_NOT_EQUALS = (
+        r"!=|"
+        r"!=="
+        )
 
 # RegEx for SEPERATORS
 t_SEP_SEMICOLON = r";"
-t_SEP_OPEN_BRACE = r"{"
-t_SEP_CLOSE_BRACE = r"{"
+t_SEP_OPEN_BRACE = r"\{"
+t_SEP_CLOSE_BRACE = r"\}"
+t_SEP_OPEN_BRACKET = r"\["
+t_SEP_CLOSE_BRACKET = r"\]"
+t_SEP_QUOTES = (
+        r"\"|"
+        r"\'"
+        )
 
 # RegEx for WHITESPACE
 def t_WHITESPACE(t): 
