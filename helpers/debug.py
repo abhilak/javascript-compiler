2c0d81b02d82b54a8adef6cc0350c9fe71b29e24
Srijan R Shetty <srijan.shetty@gmail.com>
2014-03-29 13:05:04 +0530

    debug module only contains printStatement

--- helpers/debug.py
+++ helpers/debug.py
@@ -1,15 +1,7 @@
-lineNumber = 0
 showStatement = 1
-printArguments = 1
 
 # a function to print the name of the statement
 def printStatement(statement):
     global showStatement
     if showStatement:
         print statement
-
-# a function to print the arguments of a function
-def printArguments(function_name, arguments):
-    global printArguments
-    if printArguments:
-        print 'Argument of function "', function_name, '" are:', arguments
