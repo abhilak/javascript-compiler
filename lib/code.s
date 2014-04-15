672288efae9d4f5d6b2c48bbbf3e5b369bd65d25
Srijan R Shetty <srijan.shetty@gmail.com>
2014-04-15 21:45:00 +0530

    code snippets

--- lib/code.s
+++ lib/code.s
@@ -1,23 +1,19 @@
-# Exit
 exit:
-	li 		$v0, 10 				# Preparing for exit system call
+	li 		$v0, 10
 	syscall
 
-# Sub routine for printing integer input
 print_integer:
-	li		$v0, 1 					# Preparing the system for the print_integer system call
+	li		$v0, 1 
 	syscall
 	jr		$ra
 
-# Sub routine for printing sting input
 print_string:
-	li		$v0, 4 					# Preparing the system for the print_string system call
+	li		$v0, 4
 	syscall
 	jr		$ra
 
-# Sub routine for accepting integer input
 input_integer:
-	li		$v0, 5					# Prepare the system for input_integer system call
+	li		$v0, 5
 	syscall				
-	jr		$ra						# Return to the callee
+	jr		$ra	
 
--- tests/program7.js
+++ tests/program7.js
@@ -1,3 +1,4 @@
 var y::num, z::num, p::num;
 
 y = 2;
+consolelog(y);
